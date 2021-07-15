7a007887a005e73b5966cd9428a98737061fafd8
Nadya Fernanda <84246183+nadyafsilva@users.noreply.github.com>
2021-07-15 12:34:17 -0300

    Implementação Security

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagem")
@@ -30,6 +33,12 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataPostagem = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne 
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+
 
 	public long getId() {
 		return id;
@@ -55,12 +64,23 @@ public class Postagem {
 		this.texto = texto;
 	}
 
-	public Date getData() {
+	
+	
+	public void setDataPostagem(Date dataPostagem) {
+		this.dataPostagem = dataPostagem;
+	}
+
+	public Date getDataPostagem() {
 		return dataPostagem;
 	}
 
-	public void setData(Date data) {
-		this.dataPostagem = data;
+
+	public Tema getTema() {
+		return tema;
+	}
+	
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
